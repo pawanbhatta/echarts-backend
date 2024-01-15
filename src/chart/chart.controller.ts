68ac0b339aa184f301923ff73f948387561a357f
Ashish <45767581+vattevaii@users.noreply.github.com>
2024-01-15 11:17:23 +0545

    feat: chart api

--- src/chart/chart.controller.ts
+++ src/chart/chart.controller.ts
@@ -3,6 +3,7 @@ import {
   Controller,
   Delete,
   Get,
+  HttpStatus,
   Param,
   Post,
   Put,
@@ -13,13 +14,15 @@ import { Chart } from './schemas/chart.schema';
 import { CreateChartDto } from './dto/create-chart.dto';
 import { UpdateChartDto } from './dto/update-chart.dto';
 import * as echarts from 'echarts';
-import { createCanvas } from 'canvas';
+import { Canvas, createCanvas } from 'canvas';
+import { Response } from 'express';
 // import * as fs from 'fs';
 // import { JSDOM } from 'jsdom';
 // import canvas2image from 'canvas2image';
 
 @Controller('charts')
 export class ChartController {
+  private chartData = [];
   constructor(private chartService: ChartService) {}
 
   @Get()
@@ -27,42 +30,75 @@ export class ChartController {
     return this.chartService.findAll();
   }
 
-  @Get('generate')
-  async generateChart(@Res() res) {
-    const canvas: any = createCanvas(600, 400);
-    canvas.getContext('2d');
+  @Post('data')
+  addChartData(@Res() res: Response, @Body() body) {
+    try {
+      const data = body;
+      this.chartData = data;
+      res.status(200).json(data);
+    } catch (e) {
+      console.log(e);
+      res.status(HttpStatus.FORBIDDEN).send('Data error');
+    }
+  }
 
-    const chart = echarts.init(canvas);
-    const options = {
-      title: {
-        text: 'ECharts Example',
-      },
-      xAxis: {
-        type: 'category',
-        data: [
-          'Category A',
-          'Category B',
-          'Category C',
-          'Category D',
-          'Category E',
-        ],
-      },
-      yAxis: {
-        type: 'value',
-      },
-      series: [
-        {
-          data: [120, 200, 150, 80, 70],
-          type: 'bar',
-        },
-      ],
-    };
+  @Get('generate/:id')
+  async generateChart(@Res() res: Response, @Param('id') id: string) {
+    console.log(id, this.chartData.length === 0);
+    if (!id) return res.status(404).send('No id sent.');
+    if (this.chartData.length === 0)
+      return res.status(HttpStatus.CONFLICT).send('No data found.');
+    const canvas: Canvas = createCanvas(600, 400);
+    const ctx = canvas.getContext('2d');
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fill();
+    const chart = echarts.init(canvas as unknown as HTMLElement);
 
-    chart.setOption(options);
-
-    const buffer = canvas.toBuffer('image/png');
-    res.set({ 'Content-Type': 'image/png' });
-    res.send(buffer);
+    try {
+      const options = (() => {
+        switch (id.toLowerCase()) {
+          case 'waterfall':
+            return this.chartService.generateWaterfallOptions(this.chartData);
+          case 'negative':
+            return this.chartService.generateNegativeOptions(this.chartData);
+          default:
+            return this.chartService.generateGroupedOptions(this.chartData);
+        }
+      })();
+      chart.setOption(options);
+      const buffer = canvas.toBuffer('image/png');
+      res.set({ 'Content-Type': 'image/png' });
+      res.send(buffer);
+    } catch (e) {
+      return res
+        .status(HttpStatus.FAILED_DEPENDENCY)
+        .send('Data validataion failed');
+    }
+    // const options = {
+    //   title: {
+    //     text: 'ECharts Example',
+    //   },
+    //   xAxis: {
+    //     type: 'category',
+    //     data: [
+    //       'Category A',
+    //       'Category B',
+    //       'Category C',
+    //       'Category D',
+    //       'Category E',
+    //     ],
+    //   },
+    //   yAxis: {
+    //     type: 'value',
+    //   },
+    //   series: [
+    //     {
+    //       data: [120, 200, 150, 80, 70],
+    //       type: 'bar',
+    //     },
+    //   ],
+    // };
   }
 
   @Post()
